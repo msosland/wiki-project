3b2e2305f38309a1ab37099cdeed56447a79a32e
Marshall Sosland <email": null>
2016-03-08 16:27:33 -0600

    add null false for database columns

--- db/migrate/20160307193353_create_articles.rb
+++ db/migrate/20160307193353_create_articles.rb
@@ -1,14 +1,14 @@
 class CreateArticles < ActiveRecord::Migration
   def change
     create_table :articles do |t|
-      t.string :title
+      t.string :title, null: false
       t.text :content
       t.boolean :featured
       t.string :footnotes
       t.text :bibliography
       t.string :status
-      t.integer :author_id
-      t.integer :category_id
+      t.integer :author_id, null: false
+      t.integer :category_id, null: false
 
       t.timestamps null: false
     end
