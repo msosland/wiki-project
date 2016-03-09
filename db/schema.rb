ca6d2d1becd12a64c226c9fa7a944ed46244727f
James O'Neal & James Artz <pair+jamesponeal.jelliotartz@devbootcamp.com>
2016-03-08 20:10:59 -0600

    adds publish/unpublish and marking as needs sources functionality

--- db/schema.rb
+++ db/schema.rb
@@ -11,19 +11,20 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160308221646) do
+ActiveRecord::Schema.define(version: 20160307194339) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
   create_table "articles", force: :cascade do |t|
-    t.string   "title",       null: false
+    t.string   "title",                                 null: false
     t.boolean  "featured"
-    t.string   "status"
-    t.integer  "author_id",   null: false
-    t.integer  "category_id", null: false
-    t.datetime "created_at",  null: false
-    t.datetime "updated_at",  null: false
+    t.string   "status",        default: "unpublished"
+    t.boolean  "needs_sources", default: false
+    t.integer  "author_id",                             null: false
+    t.integer  "category_id",                           null: false
+    t.datetime "created_at",                            null: false
+    t.datetime "updated_at",                            null: false
   end
 
   create_table "categories", force: :cascade do |t|
@@ -45,13 +46,13 @@ ActiveRecord::Schema.define(version: 20160308221646) do
   create_table "users", force: :cascade do |t|
     t.string   "username",                              null: false
     t.string   "password_digest"
+    t.string   "status",             default: "member"
     t.datetime "created_at",                            null: false
     t.datetime "updated_at",                            null: false
     t.string   "image_file_name"
     t.string   "image_content_type"
     t.integer  "image_file_size"
     t.datetime "image_updated_at"
-    t.string   "status",             default: "member"
   end
 
   create_table "versions", force: :cascade do |t|
